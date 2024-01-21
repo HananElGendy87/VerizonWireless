d55e6b5ff57d930b12ca2c8dd92aea448490de9c
Hanan_Cydeo <Hanan_Cydeo>
2024-01-21 00:46:16 -0600

    packages

---
+++ src/test/java/com/verizonWireless/pages/HomePage.java
@@ -0,0 +1,28 @@
+package com.verizonWireless.pages;
+import com.verizonWireless.utility.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+
+public class HomePage {
+
+    public HomePage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+        @FindBy(xpath = "//a[@class='elementor-item'][1]")
+                public static WebElement contactUsModule;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
